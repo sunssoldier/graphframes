9d3baa3e2b7e1f1b1b97d8eea976eee3e14a82d8
sunssoldier <sunssoldier@gmail.com>
2021-12-02 22:41:35 -0500

    add data and script to get data

---
+++ data/get-data.sh
@@ -0,0 +1,2 @@
+wget https://github.com/databricks/Spark-The-Definitive-Guide/blob/master/data/bike-data/201508_station_data.csv
+wget https://github.com/databricks/Spark-The-Definitive-Guide/blob/master/data/bike-data/201508_trip_data.csv
